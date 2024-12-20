22506cdb2d3c78e97a5845e01cfb3509b744189d
CometB <126880018+CometB@users.noreply.github.com>
2024-12-20 16:19:44 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+int main(){
+    srand(time(0));
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    
+    int x;
+    for(int i=0;i<3;i++){
+        cin >> x;   
+    }
+    
+    cout << "You will get " << grade[rand() % 9] << " in this 261102.";    
+}
+
